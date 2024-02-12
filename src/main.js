3418074a07fa0c791d25ae07817b3597da56920a
Adithya Ajay <113968431+Adithya1507@users.noreply.github.com>
2024-02-12 18:47:09 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 //import * as functions from '@appwrite/functions';
-import { Client } from 'node-appwrite';
-import * as sdk from 'node-appwrite';
+// import { Client } from 'node-appwrite';
+// import * as sdk from 'node-appwrite';
 
-// Initialize Appwrite SDK
-const client = new sdk.Client();
-client.setEndpoint("https://cloud.appwrite.io/v1"); // Set your Appwrite endpoint
-client.setProject('65aa01cb9e240bfad45d'); // Set your Appwrite project ID
-client.setKey('00e06217789ec3ac74a1a426ac2d10858ae676ad0c223ad43177ca9f7336d16ecf21e4637c0a205b82275f1b8725e6134321d661da30b706f50d642242c6a66b39d39c2aa74bb01a0f4a2ff81646856d000149e80eb499e3fbbfc5750322bf2787d40d059148de25095135cbdcc7471ea459f190dc476ddfc467bc6ba03385be'); // Set your Appwrite API key
+// // Initialize Appwrite SDK
+// const client = new sdk.Client();
+// client.setEndpoint("https://cloud.appwrite.io/v1"); // Set your Appwrite endpoint
+// client.setProject('65aa01cb9e240bfad45d'); // Set your Appwrite project ID
+// client.setKey('00e06217789ec3ac74a1a426ac2d10858ae676ad0c223ad43177ca9f7336d16ecf21e4637c0a205b82275f1b8725e6134321d661da30b706f50d642242c6a66b39d39c2aa74bb01a0f4a2ff81646856d000149e80eb499e3fbbfc5750322bf2787d40d059148de25095135cbdcc7471ea459f190dc476ddfc467bc6ba03385be'); // Set your Appwrite API key
 
 export async function onDocumentUpdate(event) {
   try {
