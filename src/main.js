53b74514cff941583393e542960c9aa431c13417
Adithya Ajay <113968431+Adithya1507@users.noreply.github.com>
2024-02-12 12:23:35 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,24 +1,36 @@
 const functions = require('@appwrite/functions');
+const sdk = require('node-appwrite');
 
 // Initialize Appwrite SDK
-const client = new functions.Client();
+const client = new sdk.Client();
+client.setEndpoint("https://cloud.appwrite.io/v1"); // Replace 'YOUR_ENDPOINT' with your Appwrite endpoint
+client.setProject('65aa01cb9e240bfad45d'); // Replace 'YOUR_PROJECT_ID' with your Appwrite project ID
+client.setKey('00e06217789ec3ac74a1a426ac2d10858ae676ad0c223ad43177ca9f7336d16ecf21e4637c0a205b82275f1b8725e6134321d661da30b706f50d642242c6a66b39d39c2aa74bb01a0f4a2ff81646856d000149e80eb499e3fbbfc5750322bf2787d40d059148de25095135cbdcc7471ea459f190dc476ddfc467bc6ba03385be'); // Replace 'YOUR_API_KEY' with your Appwrite API key
 
-exports.onDocumentChange = functions.event.subscribe('database.*.documents.*', async (event) => {
+
+exports.onDocumentUpdate = functions.event.subscribe('database.*.documents.update', async (event) => {
   try {
-    // Extract collection and document IDs from event data
+    console.log("iiiiiiiiiiiii21434354");
     const collectionId = event.$collection;
     const documentId = event.$id;
 
-    // Check if the event is for "Collection1" and valid for create/update
-    if (collectionId === '65c9a8d2705210df628f' && (event.$eventType === 'document.create' || event.$eventType === 'document.update')) {
-      // Get the document
-      const document = await client.database.getDocument(collectionId, documentId);
+    // Check if the event is for "collection1"
+    if (collectionId === '65c9a8d2705210df628f') {
+      // Access the updated document data
+      const updatedDocument = await client.database.getDocument(collectionId, documentId);
 
-      // Update the "name" field
-      document.data.name = 'adithyatest';
-      console.log('Function execution completed1234');
-      // Save the updated document
-      await client.database.updateDocument(collectionId, documentId, document.data);
+      // Check if "name" field exists
+      if (updatedDocument.hasOwnProperty('name')) {
+        // Update the "name" field
+        updatedDocument.name = 'adi';
+
+        // Save the updated document
+        await client.database.updateDocument(collectionId, documentId, updatedDocument);
+
+        console.log(`Document "${documentId}" in collection "${collectionId}" updated to "adi".`);
+      } else {
+        console.log(`Document "${documentId}" in collection "${collectionId}" doesn't have "name" field. Skipping update.`);
+      }
     }
   } catch (error) {
     console.error(error);
