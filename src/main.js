5991cff9572034169ab93e692d740ee0a9efd316
Adithya Ajay <141824002+Adithya-AntPhy@users.noreply.github.com>
2024-02-13 12:07:12 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -52,7 +52,7 @@ export default async ({ req, res, log, error }) => {
   // You can log messages to the console
   //log('Hello, Logs123! ' + JSON.stringify(req.body.$collectionId));
   const collectionModified= req.body.$collectionId
-  const documentModified=JSON.stringify(req.body.$id)
+  const documentModified=req.body.$id
   //log("document modified"+ documentModified)
   //log("collectionModified:"+ collectionModified)
    if(collectionModified === "65c9a8d2705210df628f")
